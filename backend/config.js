d7c29cc6da827ccc971953f20b99b555b638239e
ominema12 <ominema12@gmail.com>
2023-12-14 14:03:45 +0530

    database setup using postman

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,5 @@
 export const PORT=5555;
 
 export const mongoDBURL=
-'mongodb+srv://ominema12:<password>@cluster0.nwu1f83.mongodb.net/books-collection?retryWrites=true&w=majority'
+'mongodb+srv://omi:omi@cluster1.usvlh4m.mongodb.net/books-collection'
+// 'mongodb+srv://ominema12:<password>@cluster0.nwu1f83.mongodb.net/books-collection?retryWrites=true&w=majority'
--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,56 @@
 
 import express from "express";
-import {PORT,mongoDBURL} from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
-import {Book} from './models/bookModel.js';
+import { Book } from './models/bookModel.js';
 
-const app=express();
+const app = express();
 
-app.get('/',(request,response)=>{
+//Middleware for parsing request body
+app.use(express.json());
+
+//get to fetch data from dtabase
+app.get('/', (request, response) => {
     console.log(request);
     return response.status(234).send('Welcome To MERN Stack');
 });
 
 //Route for Save a new Book
-app.post('/books',async(request,response)=> {
-try{
-if(|| !request.body.publishYear
-)
-}
-catch(error){
-    console.log(error.message);
-    response.status(500).send({message:error.message});
-}
+//post to give to database
+app.post('/books', async (request, response) => {
+    try {
+        if (
+            !request.body.title || !request.body.author || !request.body.publishYear
+        ) {
+            return response.status(400).send({
+                message: 'Send all required feilds: title,author, publishYear',
+            });
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.PublishYear,
+        };
+        const book = await Book.create(newBook);
+        return response.status(201).send(book);
+    }
+    catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
 });
 
+
+//Route for Getting All books from Database
+
 mongoose
-  .connect(mongoDBURL)
-  .then(() => {
-    console.log('App connected to database');
-    app.listen(PORT, () => {
-      console.log(`App is listening to port: ${PORT}`);
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('App connected to database');
+        app.listen(PORT, () => {
+            console.log(`App is listening to port: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error);
     });
-  })
-  .catch((error) => {
-    console.log(error);
-  });
