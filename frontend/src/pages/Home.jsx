a863e00a0222cc81d491223e394ffba679046861
ominema12 <ominema12@gmail.com>
2023-12-16 01:34:27 +0530

    home table layout

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,25 +7,38 @@ import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 
-const Home = () => {
-  const [books, setBooks] = useState([]);
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    axios
-      .get('http://loacalhost:5555/books')
-      .then(response)=> {
-  setLoading(response.data.data);
-  setLoading(false);
-   )
-}
-   .catch ((error) => {
-  console.log(error);
+const [books, setBooks] = useState([]);
+const [loading, setLoading] = useState(false);
+//const [showType, setShowType] = useState('table');
+
+useEffect(() => {
+  setLoading(true);
+  axios
+    .get('http://localhost:5555/books')
+    .then((response) => {
+      setBooks(response.data.data);
+      setLoading(false);
+    })
+    .catch((error) => {
+      console.log(error);
+      setLoading(false);
+    });
 }, []);
-   });
+
 return (
-  <div>Home</div>
+  <div className='p-4'>
+    <div className='flex justify-center items-center gao-x-4'>
+      <button className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg' onClick={() => setShowType('table')}>
+        Table
+      </button>
+      <button
+        className='bg-sky-300 hover:bg-sky-600 px-4 py-1 rounded-lg'
+        onClick={() => setShowType('card')}>
+        Card
+      </button>
+
+    </div>Home</div>
 )
-}
+
 
 export default Home;
