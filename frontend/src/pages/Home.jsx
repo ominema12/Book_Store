b4e38d948634565c1fdb32254c9251a1724684c5
ominema12 <ominema12@gmail.com>
2023-12-15 23:58:29 +0530

    home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,31 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import spinner from '../components/spinner';
-import {Link} from 'react-router-dom';
-import {AiOutlineEdit} from 'react-icons/ai';
-import {BsInfoCircle} from 'react-icons/bs';
-import {MdOutlineAddBox,MdOutlineDelete} from 'react-icons/md';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get('http://loacalhost:5555/books')
+      .then(response)=> {
+  setLoading(response.data.data);
+  setLoading(false);
+   )
+}
+   .catch ((error) => {
+  console.log(error);
+}, []);
+   });
+return (
+  <div>Home</div>
+)
 }
 
 export default Home;
