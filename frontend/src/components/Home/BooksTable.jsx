595445ed722dceb23f206105cd64a42fcbe88b48
ominema12 <ominema12@gmail.com>
2023-12-16 01:49:49 +0530

    dome books assemble in home

--- frontend/src/components/Home/BooksTable.jsx
+++ frontend/src/components/Home/BooksTable.jsx
@@ -1,45 +1,57 @@
-import { AiOutlineClose } from 'react-icons/ai';
-import { PiBookOpenTextLight } from 'react-icons/pi';
-import { BiUserCircle } from 'react-icons/bi';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
-const BookModal = ({ book, onClose }) => {
+const BooksTable = ({ books }) => {
   return (
-    <div
-      className='fixed bg-black bg-opacity-60 top-0 left-0 right-0 bottom-0 z-50 flex justify-center items-center'
-      onClick={onClose}
-    >
-      <div
-        onClick={(event) => event.stopPropagation()}
-        className='w-[600px] max-w-full h-[400px] bg-white rounded-xl p-4 flex flex-col relative'
-      >
-        <AiOutlineClose
-          className='absolute right-6 top-6 text-3xl text-red-600 cursor-pointer'
-          onClick={onClose}
-        />
-        <h2 className='w-fit px-4 py-1 bg-red-300 rounded-lg'>
-          {book.publishYear}
-        </h2>
-        <h4 className='my-2 text-gray-500'>{book._id}</h4>
-        <div className='flex justify-start items-center gap-x-2'>
-          <PiBookOpenTextLight className='text-red-300 text-2xl' />
-          <h2 className='my-1'>{book.title}</h2>
-        </div>
-        <div className='flex justify-start items-center gap-x-2'>
-          <BiUserCircle className='text-red-300 text-2xl' />
-          <h2 className='my-1'>{book.author}</h2>
-        </div>
-        <p className='mt-4'>Anything You want to show</p>
-        <p className='my-2'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quia
-          voluptatum sint. Nisi impedit libero eveniet cum vitae qui expedita
-          necessitatibus assumenda laboriosam, facilis iste cumque a pariatur
-          nesciunt cupiditate voluptas? Quis atque earum voluptate dolor nisi
-          dolorum est? Deserunt placeat cumque quo dicta architecto, dolore
-          vitae voluptate sequi repellat!
-        </p>
-      </div>
-    </div>
+    <table className='w-full border-separate border-spacing-2'>
+      <thead>
+        <tr>
+          <th className='border border-slate-600 rounded-md'>No</th>
+          <th className='border border-slate-600 rounded-md'>Title</th>
+          <th className='border border-slate-600 rounded-md max-md:hidden'>
+            Author
+          </th>
+          <th className='border border-slate-600 rounded-md max-md:hidden'>
+            Publish Year
+          </th>
+          <th className='border border-slate-600 rounded-md'>Operations</th>
+        </tr>
+      </thead>
+      <tbody>
+        {books.map((book, index) => (
+          <tr key={book._id} className='h-8'>
+            <td className='border border-slate-700 rounded-md text-center'>
+              {index + 1}
+            </td>
+            <td className='border border-slate-700 rounded-md text-center'>
+              {book.title}
+            </td>
+            <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+              {book.author}
+            </td>
+            <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
+              {book.publishYear}
+            </td>
+            <td className='border border-slate-700 rounded-md text-center'>
+              <div className='flex justify-center gap-x-4'>
+                <Link to={`/books/details/${book._id}`}>
+                  <BsInfoCircle className='text-2xl text-green-800' />
+                </Link>
+                <Link to={`/books/edit/${book._id}`}>
+                  <AiOutlineEdit className='text-2xl text-yellow-600' />
+                </Link>
+                <Link to={`/books/delete/${book._id}`}>
+                  <MdOutlineDelete className='text-2xl text-red-600' />
+                </Link>
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
-export default BookModal;
+export default BooksTable;
