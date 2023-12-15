ff78fe432abe54da616254a4107fcc66c4334fd8
ominema12 <ominema12@gmail.com>
2023-12-15 13:27:32 +0530

    del book and edit book

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,8 +12,8 @@ const App = () => {
       <Route path='/' element={ <Home />} />
       <Route path='/books/create' element={ <CreateBooks />} />
       <Route path='/books/details/:id' element={ <ShowBook /> } />
-      <Route path='' element={ } />
-      <Route path='' element={ } />
+      <Route path='/books/edit/:id' element={ <EditBook /> } />
+      <Route path='/books/delete/:id' element={ <DeleteBook /> } />
     </Routes>
   )
 }
