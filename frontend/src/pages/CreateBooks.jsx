3511c5297dbefdd3197dbcc3a332c9d6031b9ddf
ominema12 <ominema12@gmail.com>
2023-12-16 20:21:38 +0530

    creating books

--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,6 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import BackButton from '../components/BackButton';
+import Spinner from '../components/Spinner';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useSnackbar } from 'notistack';
 
 const CreateBooks = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [publishYear, setPublishYear] = useState('');
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const { enqueueSnackbar } = useSnackbar();
+
+
+
+
+
+
+
+
+
   return (
     <div>CreateBooks</div>
   )
